5020d44e541d74e394a55b2b44275f70de8a97bc
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-11-08 14:46:52 +0600

    generics

---
+++ Module2/2.3.ts
@@ -0,0 +1,36 @@
+{
+
+
+
+    // type GenericArray = Array <boolean|string|number>;
+    type GenericArray<T> = Array<T>;
+
+
+    //generic type
+    const rollNumbers: GenericArray<number> = [3, 6, 8];
+
+    // const mentors : string [] =["x","y", "z"]
+    const mentors: GenericArray<string> = ["x", "y", "z"]
+
+    // const boolArray:boolean[]=[true,false,true]
+    const boolArray: GenericArray<boolean> = [true, false, true];
+
+    const user: GenericArray<{ name: string, age: number }> = [
+        {
+            name: 'Raqib',
+            age: 100
+
+        }, {
+            name: "Jhankar vai",
+            age: 150
+        }]
+
+
+        //generic tuple 
+        type GenericTuple<X,Y>=[X,Y]
+
+        const manus:GenericTuple<string,string> =['X','Y']
+
+        const UserWithID:GenericTuple<number,{name:string, email:string}> = [12345,{name:"Raqib", email:"raqib1345@gmail.com"}]
+
+}
