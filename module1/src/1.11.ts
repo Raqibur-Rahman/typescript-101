894bb38fd637040665fbd2cdc85f6688ba1a8ab6
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-11-08 10:41:54 +0600

    m2 creation, alias, type assertion

---
+++ module1/src/1.11.ts
@@ -0,0 +1,21 @@
+{
+    //ternary operator || optional chaining || nullish coalescing operator
+
+    const age : number = 25;
+
+
+
+    // if(age>=18){
+    //     console.log("allowed");
+    // }
+    // else{
+    //     console.log("not allowed")
+    // }
+
+    const isAdult = age >= 18 ? "allowedT":"not allowedT";
+    console.log(isAdult);
+
+
+
+
+}
